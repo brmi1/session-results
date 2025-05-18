a7f0732dc40a011a2250b9c78a669cdf19b30087
brmi1 <myfixby>
2025-05-19 02:43:19 +0300

    Добавлены начальные данные

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     cout << "Session results #8. GIT\n";
-    cout << "Variant #0. Session results\n";
+    cout << "Variant #8. Session results\n";
     cout << "Author: Roman Bachun\n";
     return 0;
 }
