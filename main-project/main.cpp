4b542b3f0c7dc73a221e93d336a7e86ecdc903d9
brmi1 <myfixby>
2025-05-19 18:03:40 +0300

    Добавлены тестовые данные

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -7,5 +7,6 @@ int main()
     cout << "Session results #8. GIT\n";
     cout << "Variant #8. Session results\n";
     cout << "Author: Roman Bachun\n";
+    cout << "Groupt: 24Pinj\n";
     return 0;
 }
