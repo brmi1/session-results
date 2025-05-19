666ed7864cc085c400fdb5f61777c64a49195b90
brmi1 <myfixby>
2025-05-19 17:22:06 +0300

    Реализовано форматирование данных при выводе в консоль

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -12,7 +12,7 @@ int main()
     setlocale(LC_ALL, "Russian");
     cout << "Session results #8. GIT\n";
     cout << "Variant #8. Session results\n";
-    cout << "Author: Roman Bachun\n";
+    cout << "Author: Roman Bachun\n\n";
     session_results* session_results[MAX_FILE_ROWS_COUNT];
     int size;
     try
@@ -20,15 +20,14 @@ int main()
         read("data.txt", session_results, size);
         for (int i = 0; i < size; i++)
         {
-            cout << session_results[i]->student.last_name << '\n';
-            cout << session_results[i]->student.first_name << '\n';
-            cout << session_results[i]->student.middle_name << '\n';
+            cout << session_results[i]->student.last_name << ' ';
+            cout << session_results[i]->student.first_name << ' ';
+            cout << session_results[i]->student.middle_name << ' ';
             cout << session_results[i]->date.day << '.';
             cout << session_results[i]->date.month << '.';
-            cout << session_results[i]->date.year << '\n';
-            cout << session_results[i]->grade << '\n';
+            cout << session_results[i]->date.year << ' ';
+            cout << session_results[i]->grade << ' ';
             cout << session_results[i]->subject << '\n';
-            cout << '\n';
         }
         for (int i = 0; i < size; i++)
         {
